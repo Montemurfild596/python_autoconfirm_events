0a5af832ba9e1013576640d8d1305fc55253bd37
Omutnykh Oleg <omutnih1234@gmail.com>
2023-07-14 12:03:56 +0500

    initial commit

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ import time
 from timepad.rest import ApiException
 #from timepad.configuration import Configuration
 from timepad import rest, configuration, Api, api_client, api
+from timepad.api import DefaultApi
+from timepad.configuration import Configuration
+from timepad.api_client import ApiClient
 
-configuration1 = configuration.Configuration
+configuration1 = Configuration()
 configuration1.access_token = 'c08ef390e759340fc86bd903ca8906894b59b1a4'
 
-api_instance = api.DefaultApi(api_client.ApiClient(configuration))
+api_instance = DefaultApi(ApiClient(configuration1))
 
 
 app = Flask(__name__)
 
 
 
-@app.route('/webhook', methods = ['POST'])
+@app.route('/', methods = ['POST'])
 def webhook():
     if request.method == 'POST':
         json_request = request.json
@@ -24,7 +27,7 @@ def webhook():
                 str_order_id = json_request["order_id"]
                 event_id = json_request["event_id"]
                 api_instance.approve_event_order(event_id, int(str_order_id))
-                return ('success', 200)
+            return ('success', 200)
         except ApiException as e:
             print("Exception when calling DefaultApi->approve_event_order: %s\n" % e)
             abort(400)
