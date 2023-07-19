c60f903580c6ffce3f268a9146daca0e0291c2b6
Omutnykh Oleg <omutnih1234@gmail.com>
2023-07-19 14:52:40 +0500

    Удалены тестовые файлы

--- webhook.py
+++
@@ -1,8 +0,0 @@
-import requests, json
-
-webhook_url = 'http://127.0.0.1:5000/webhook'
-
-data = { "name": "DevOps Journey",
-         "channel URL": "test url" }
-
-r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
